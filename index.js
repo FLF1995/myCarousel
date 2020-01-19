1907eca9ccd43a6565c7b9d03181f6219444104a
FLF1995 <carolfan17@163.com>
2020-01-19 17:04:12 +0800

    js for carousel

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
-function changeImgs() {
+var curImg = document.getElementById('img')
+var num = 1
+var dotsArr = document.getElementById("dotsList")[0].getElementsByTagName("li");
 
+function changeImgs() {
+	setInterval(() => {
+    moveToRight();
+  }, 2000);
 }
 
 function moveToRight() {
-	console.log(1111)
+	num++;
+	if(num == 4) {
+		num = 1
+	}
+	curImg.src = `./images/img${num}.jpg`;
 }
 
-changeImgs();
+function moveToLeft() {
+	num--;
+	if(num == 0) {
+		num = 3
+	}
+	curImg.src = `./images/img${num}.jpg`;
+}
+
+function clickDots() {
+	for (var i = 0; i < dotsArr.length; i++) {
+    dotsArr[i].index = i;
+    dotsArr[i].onclick = function() {
+      var num = this.index + 1;
+      curImg.src = "img/" + num + ".jpg";
+    };
+  }
+}
+
+// changeImgs();
